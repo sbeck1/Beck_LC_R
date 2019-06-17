460ffc245898a96d36a71d5881dcd02018e4e622
Beck <stevenbeck@ufl.edu>
2019-06-17 09:01:47 -0400

    continue with analysis

--- Oyster/Spat/script/tile_summary.R
+++ Oyster/Spat/script/tile_summary.R
@@ -1,22 +1,21 @@
 library (ggplot2)
-library (Rmisc) #multiplot
 library (dplyr)
 library (plotly)
 
 tile_spat=read.csv("oyster/spat/data/development/tile_spat_count.csv")
 tile_spat2=tile_spat[which(tile_spat$cnt>=0),]
 
-#surface area of tile = (2*(0.151m x 0.151m))+(4*(0.151 x 0.101m)) = 0.106606sq m
+#surface area of tile = 
 
 #format for merge
 tile_summary = tile_spat2 %>%
   group_by(station,month,year)%>%
-  summarise(tile_total=sum(cnt))
+  summarise(tile_total=sum(cnt))  # multiple by xxxx to obtain spat per meter
 
 #format for rbind
 tile_summary2 = tile_spat2 %>%
   group_by(station,month,year)%>%
-  summarise(total=sum(cnt)) #multiply total spat count by 9.380335 to obtain total spat per sq m
+  summarise(total=sum(cnt)) 
 tile_summary2$type="TILE"
 str(tile_summary2)
 
@@ -28,8 +27,8 @@ tile_summary2$year=as.factor(tile_summary2$year)
 tile_summary2$station=factor(tile_summary2$station,levels=c("WQ6","WQ1","WQ7","WQ5","WQ2","WQ8","WQ4","WQ3","WQ9","WQ10"))
 
 ggplot(tile_summary2, aes(x=month_num,y=total,shape=year))+
-  geom_point(size=2.5)+
-  labs(title="Tile Spat Collectors:  Total Spat by Station/Month",x="Month", y="Spat/Tile")+
+  geom_point(size=2)+
+  labs(title="Tile Spat Collectors:  Total Spat by Station/Month/Year",x="Month", y="Spat per Tile")+
   facet_wrap(~station, ncol=3)+
   theme(axis.text.x=element_text(size=rel(0.8)))
 
