b4c3e08d05a8a938476d3afe0cea7b0155a0cef2
Beck <stevenbeck@ufl.edu>
2019-10-27 16:01:32 -0400

    update

--- Oyster/transect/script/transect_doublecount_draw.R
+++ Oyster/transect/script/transect_doublecount_draw.R
@@ -1,3 +1,5 @@
+#### random draw for which transect to doublecount ####
+
 trans_double=read.csv("oyster/transect/data/transect_draw_final_2019.csv")
 trans_double=trans_double[which(trans_double$TYPE=="TRAN_ST"),]
 trans_double=trans_double[which(trans_double$QUESTION==2),]
@@ -6,3 +8,9 @@ trans_double_rand=sample(nrow(trans_double))
 trans_double2=trans_double[trans_double_rand,1:11]
 91*.2 # 20% of transects doublecount per 2018-2019 = 18
 write.csv(trans_double2, file="oyster/transect/data/trans_dblcnt_2019.csv")
+
+#### random draw for which segment of each transect to measure oyster heights  ####
+
+rand_seg=read.csv("oyster/transect/data/rand_seg.csv")
+rand_seg2=sample(rand_seg$segment,150,replace=T)
+as.data.frame(rand_seg2)
